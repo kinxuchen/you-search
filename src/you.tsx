18b895f585badc09f83e29ddbbb69a676ba3cb67
xuchen <kingxc1991@foxmail.com>
2024-01-28 20:21:37 +0800

    feat: 添加 model 选择, 需要配合浏览器插件使用

--- src/you.tsx
+++ src/you.tsx
@@ -4,21 +4,41 @@
 // getPreferenceValues：这是一个函数，用于获取用户的偏好设置。
 // open：这是一个函数，用于打开一个 URL。
 // popToRoot：这是一个函数，用于在提交表单后关闭所有打开的面板，返回到 Raycast 的主屏幕
-import { Action, ActionPanel, Form, LaunchProps, getPreferenceValues, open, popToRoot } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  Form,
+  LaunchProps,
+  getPreferenceValues,
+  open,
+  popToRoot,
+  LocalStorage,
+} from "@raycast/api";
+import { useState, useEffect } from 'react';
 import { useForm } from "@raycast/utils";
 
+interface ModelProps {
+  model: 'gpt-4' | 'research' | 'create' | 'agent' | 'default';
+}
 type Values = {
   query: string;
+  model: ModelProps['model'];
 };
 
+
+// 返回某个命令
 export default function Command(props: LaunchProps<{ draftValues: Values; arguments: Values }>) {
-  const { handleSubmit, itemProps } = useForm<Values>({
-    onSubmit(params) {
-      popToRoot();
-      open(`https://you.com/search?q=${params.query}&tbm=youchat`);
+  const defaultModel = getPreferenceValues<ModelProps>().model
+  const [model, setModel] = useState<ModelProps['model'] | undefined>(undefined);
+  const { handleSubmit, itemProps, setValue, values } = useForm<Values>({
+    async onSubmit(params) {
+      await popToRoot();
+      await open(`https://you.com/search?q=${params.query}&tbm=youchat&chat_mode=${params.model}`);
+      await LocalStorage.setItem('model', params.model);
     },
     initialValues: {
       query: props.draftValues?.query ?? props.fallbackText ?? "",
+      model: defaultModel
     },
     validation: {
       query: (value) => (value && value.length > 0 ? null : "Query cannot be empty"),
@@ -28,6 +48,7 @@ export default function Command(props: LaunchProps<{ draftValues: Values; argume
   if (props.arguments.query) {
     handleSubmit({
       query: props.arguments.query,
+      model: props.arguments.model || defaultModel
     });
     return null;
   }
@@ -35,10 +56,27 @@ export default function Command(props: LaunchProps<{ draftValues: Values; argume
   if (props.fallbackText) {
     handleSubmit({
       query: props.fallbackText as string,
+      model: props.arguments.model || defaultModel
     });
     return null;
   }
 
+  useEffect(() => {
+    LocalStorage.getItem('model').then(m => {
+      if (typeof  m === 'string') {
+        setValue('model', () => {
+          return m as ModelProps['model'];
+        });
+        setModel(m as ModelProps['model'])
+        LocalStorage.setItem('model', m);
+      } else {
+        setValue('model', () => {
+          return defaultModel;
+        })
+        setModel(defaultModel)
+      }
+    })
+  }, [])
   return (
     <Form
       enableDrafts
@@ -50,6 +88,20 @@ export default function Command(props: LaunchProps<{ draftValues: Values; argume
     >
       <Form.Description text="请使用 you 进行搜索" />
       <Form.TextArea title="Ask Anything" {...itemProps.query} />
+      {
+        model && (
+          <Form.Dropdown id="model" value={model} title="Please select the model" onChange={val => {
+            LocalStorage.setItem('model', val);
+            setModel(val as ModelProps['model'])
+          }}>
+            <Form.DropdownItem value="default" title="Smart" />
+            <Form.DropdownItem value="agent" title="Genius" />
+            <Form.DropdownItem value="gpt-4" title="GPT-4" />
+            <Form.DropdownItem value="research" title="research" />
+            <Form.DropdownItem value="create" title="create" />
+          </Form.Dropdown>
+        )
+      }
     </Form>
   );
 }
